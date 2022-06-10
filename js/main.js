ec49f92fdcb043a1f13812c7b49446026c6d2b5f
inoris7 <97662782+inoris7@users.noreply.github.com>
2022-06-10 11:40:02 +0300

    функции преобразованы в стрелочные

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 //Функкция выдачи случайного числа из заданного диапазона, включая начальное и конечное значение
-function getRandomNumber (startOfRange, endOfRange) {
+const getRandomNumber = (startOfRange, endOfRange) => {
 
   //Проверка границ диапазона на то, что они не отрицательные и начало больше конца
   if(startOfRange < 0 || endOfRange < 0 || startOfRange >= endOfRange) {
@@ -10,18 +10,11 @@ function getRandomNumber (startOfRange, endOfRange) {
   startOfRange = Math.ceil(startOfRange);
   endOfRange = Math.floor(endOfRange);
   return Math.floor(Math.random() * (endOfRange - startOfRange + 1)) + startOfRange;
-}
+};
 
 getRandomNumber(1,8);
 
 //Функция сравнения длины строки
-function compareStringLenght (text, symbolQuantity) {
-
-  if(text.lenght <= symbolQuantity) {
-    return true;
-  }
-
-  return false;
-}
+const compareStringLenght = (text, symbolQuantity) => (text.lenght <= symbolQuantity);
 
 compareStringLenght ('dasdja', 8);
