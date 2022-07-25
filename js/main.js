227a00f9e165c6d7d527cc80932beb5881ca3335
inoris7 <97662782+inoris7@users.noreply.github.com>
2022-07-25 19:26:23 +0300

    Выполнена домашка 11.1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 import './render-pictures.js';
 import './upload-form.js';
-import './upload-comment-validation.js';
-import './upload-hashtag-validation.js';
+import './form-validation.js';
 import './scale-upload-preview.js';
 import './img-preview-effects.js';
+import {renderSimilarPhotos} from './render-pictures.js';
+import {closeUploadForm} from './upload-form.js';
+import {setUploadFormSubmit} from './form-validation.js';
+
+fetch ('https://26.javascript.pages.academy/kekstagram/data')
+  .then((response) => response.json())
+  .then((userPhotos) => {
+    renderSimilarPhotos(userPhotos);
+  });
+
+setUploadFormSubmit(closeUploadForm);
