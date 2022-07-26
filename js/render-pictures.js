3504e6d9be5445cf35fc091bb1c4002a97d9c23a
inoris7 <97662782+inoris7@users.noreply.github.com>
2022-07-26 16:12:32 +0300

    Сделана сортировка пользовательских фото

--- js/render-pictures.js
+++ js/render-pictures.js
@@ -5,23 +5,65 @@ const userPhotoList = document.querySelector('.pictures');
 //находим шаблон для вставки пользовательских фото
 const userPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture');
 //Генерируем массив пользовательских фото
+const similarPhotoListFragment = document.createDocumentFragment();
+const imgSorting = document.querySelector('.img-filters');
+const filterDiscussedButton = document.querySelector('#filter-discussed');
+const filterDefaultButton = document.querySelector('#filter-default');
+const filterRandomButton = document.querySelector('#filter-random');
+const filterButtons = document.querySelectorAll('.img-filters__button');
+
+
+
+const similarPhotoCreate = (photo) => {
+  similarPhotoListFragment.innerHTML = '';
+  const similarPhotoElement = userPhotoTemplate.cloneNode(true);
+  similarPhotoElement.querySelector('.picture__img').src = photo.url;
+  similarPhotoElement.querySelector('.picture__likes').textContent = photo.likes;
+  similarPhotoElement.querySelector('.picture__comments').textContent = photo.comments.length;
+  similarPhotoListFragment.appendChild(similarPhotoElement);
+
+  similarPhotoElement.addEventListener('click', () => {
+    openBigPicture(photo);
+  });
+  userPhotoList.appendChild(similarPhotoListFragment);
+};
 
 const renderSimilarPhotos = (similarPhotos) => {
-  const similarPhotoListFragment = document.createDocumentFragment();
 
-  similarPhotos.forEach((photo) => {
-    const similarPhotoElement = userPhotoTemplate.cloneNode(true);
-    similarPhotoElement.querySelector('.picture__img').src = photo.url;
-    similarPhotoElement.querySelector('.picture__likes').textContent = photo.likes;
-    similarPhotoElement.querySelector('.picture__comments').textContent = photo.comments.length;
-    similarPhotoListFragment.appendChild(similarPhotoElement);
+  imgSorting.classList.remove('img-filters--inactive');
+  similarPhotos.forEach(similarPhotoCreate);
+  userPhotoList.appendChild(similarPhotoListFragment);
 
-    similarPhotoElement.addEventListener('click', () => {
-      openBigPicture(photo);
-    });
+  filterDiscussedButton.addEventListener('click', (evt) => {
+    filterButtons.forEach((e) => e.classList.remove('img-filters__button--active'));
+    evt.target.classList.add('img-filters__button--active');
+    userPhotoList.querySelectorAll('.picture').forEach((e) => e.remove());
+
+    similarPhotos
+      .slice()
+      .sort((photoA, photoB) => photoA.likes > photoB.likes ? -1 : 1)
+      .forEach(similarPhotoCreate);
   });
 
-  userPhotoList.appendChild(similarPhotoListFragment);
+  filterRandomButton.addEventListener('click', (evt) => {
+    filterButtons.forEach((e) => e.classList.remove('img-filters__button--active'));
+    evt.target.classList.add('img-filters__button--active');
+    userPhotoList.querySelectorAll('.picture').forEach((e) => e.remove());
+
+    similarPhotos
+      .slice()
+      .sort(() => Math.random() - 0.5)
+      .forEach(similarPhotoCreate);
+  });
+
+  filterDefaultButton.addEventListener('click', (evt) => {
+    filterButtons.forEach((e) => e.classList.remove('img-filters__button--active'));
+    evt.target.classList.add('img-filters__button--active');
+    userPhotoList.querySelectorAll('.picture').forEach((e) => e.remove());
+
+    similarPhotos.forEach(similarPhotoCreate);
+
+  });
 };
 
 export {renderSimilarPhotos};
